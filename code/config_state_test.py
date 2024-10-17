a90c6b9e23852c4a5eda8d377c1b39323ea388f3
Palmer Sample <psample@cisco.com>
2024-10-17 16:42:07 -0400

    Added missing subsection_datafile link. Updated prepare_lab with caddy STDOUT redirect. Updated imports for config_state_test.py

--- code/config_state_test.py
+++ code/config_state_test.py
@@ -4,6 +4,8 @@ import logging
 from genie.harness.base import Trigger
 from pyats import aetest
 
+# Import JSON for formatted output
+import json
+
 # Set up logging
 logger = logging.getLogger(__name__)
-
